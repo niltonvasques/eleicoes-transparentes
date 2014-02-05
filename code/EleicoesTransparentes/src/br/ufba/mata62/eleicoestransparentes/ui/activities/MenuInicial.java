292afa000d93a07c09d24d0ceca81ee05f25d976
niltonvasques <niltonvasques@gmail.com>
2014-02-05 01:46:52 -0300

    Removido arquivos binarios

--- code/EleicoesTransparentes/src/br/ufba/mata62/eleicoestransparentes/ui/activities/MenuInicial.java
+++ code/EleicoesTransparentes/src/br/ufba/mata62/eleicoestransparentes/ui/activities/MenuInicial.java
@@ -1,12 +1,12 @@
 package br.ufba.mata62.eleicoestransparentes.ui.activities;
 
-import br.ufba.mata62.eleicoestransparentes.connection.EleicoesSOAP;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import br.ufba.mata62.eleicoestransparentes.connection.EleicoesSOAP;
 
 public class MenuInicial extends ListActivity {
 
