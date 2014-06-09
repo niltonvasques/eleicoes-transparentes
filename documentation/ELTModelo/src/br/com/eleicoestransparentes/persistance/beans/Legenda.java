71a8db79695b6606b8e0debc6215b08f9aef07f4
jan <jan@vm-srv.lasid.intranet.ufba.br>
2014-06-09 20:32:25 -0300

    Correção de Legenda

--- documentation/ELTModelo/src/br/com/eleicoestransparentes/persistance/beans/Legenda.java
+++ documentation/ELTModelo/src/br/com/eleicoestransparentes/persistance/beans/Legenda.java
@@ -1,8 +1,6 @@
 package br.com.eleicoestransparentes.persistance.beans;
 
 public class Legenda {
-	protected Cargo cargo;
 	protected String tipo;
-	protected Partido partido;
 	protected Coligacao coligacao;
 }
