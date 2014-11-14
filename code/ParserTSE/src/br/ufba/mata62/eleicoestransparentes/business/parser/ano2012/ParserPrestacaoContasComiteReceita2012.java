f949d54fa17d56b21263894ce211273ee4190d60
Nilton Vasques <nilton.vasques@gmail.com>
2014-11-14 15:05:57 -0300

    Adicionado o AgenteEleitoral

--- code/ParserTSE/src/br/ufba/mata62/eleicoestransparentes/business/parser/ano2012/ParserPrestacaoContasComiteReceita2012.java
+++ code/ParserTSE/src/br/ufba/mata62/eleicoestransparentes/business/parser/ano2012/ParserPrestacaoContasComiteReceita2012.java
@@ -13,6 +13,7 @@ import java.util.List;
 import br.ufba.mata62.eleicoestransparentes.business.parser.ParserFile;
 import br.ufba.mata62.eleicoestransparentes.business.parser.templates.ano2012.PrestContasComiteDespesa2012;
 import br.ufba.mata62.eleicoestransparentes.business.parser.templates.ano2012.PrestContasComiteReceita2012;
+import br.ufba.mata62.eleicoestransparentes.model.AgenteEleitoral;
 import br.ufba.mata62.eleicoestransparentes.model.Comite;
 import br.ufba.mata62.eleicoestransparentes.model.Partido;
 import br.ufba.mata62.eleicoestransparentes.model.Pessoa;
@@ -73,7 +74,7 @@ public class ParserPrestacaoContasComiteReceita2012 extends ParserFile<PrestCont
 		trans.setValor(Float.parseFloat(pccr.getValorReceita().replace(",", ".")));
 		trans.setClassificacao(pccr.getTipoReceita());
 		trans.setDescricao(pccr.getDescricaoReceita());
-//		trans.setCreditado(createComite(pccr)); TODO Comite não é uma pessoa
+		trans.setCreditado(createComite(pccr)); //TODO Comite não é uma pessoa
 		trans.setDebitado(createDoador(pccr));
 		trans.setTipo(Transacao.RECEITA);
 		trans.setUF(pccr.getUF());
@@ -81,7 +82,7 @@ public class ParserPrestacaoContasComiteReceita2012 extends ParserFile<PrestCont
 		return trans;
 	}
 	
-	private static Comite createComite(PrestContasComiteDespesa2012 pccd) {
+	private static AgenteEleitoral createComite(PrestContasComiteReceita2012 pccd) {
 		Comite com = new Comite();
 		com.setUF(pccd.getUF());
 		com.setMunicipio(pccd.getMunicipio());
@@ -89,10 +90,12 @@ public class ParserPrestacaoContasComiteReceita2012 extends ParserFile<PrestCont
 		Partido partido = new Partido();
 		partido.setSigla(pccd.getSiglaPartido());
 		com.setPartido(partido);
-		return com; 
+		AgenteEleitoral agente = new AgenteEleitoral();
+		agente.setComite(com);
+		return agente; 
 	}
 
-	private static Pessoa createDoador(PrestContasComiteReceita2012 pccr) {
+	private static AgenteEleitoral createDoador(PrestContasComiteReceita2012 pccr) {
 		Pessoa pessoa = null;
 		
 		if(ValidatorCPFCNPJ.isValidCPF(pccr.getCPFCNPJDoador())){
@@ -120,7 +123,9 @@ public class ParserPrestacaoContasComiteReceita2012 extends ParserFile<PrestCont
 			se.setNome(pccr.getSetorEconomicoDoador());
 			pessoa.setSetorEconomico(se);
 		}
-		return pessoa;
+		AgenteEleitoral agente = new AgenteEleitoral();
+		agente.setPessoa(pessoa);
+		return agente;
 	}
 	private static Date formatDate(String dateStr) {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyyhh:mm");
